41cb1cbd48db58b930b630d70c2f12eb0dd46685
Stan Taylor <staylor1@netspend.com>
2020-12-30 08:35:37 -0600

    Added findByName method

--- src/main/java/com/aphids/testplanner/repository/TestcaseRepository.java
+++ src/main/java/com/aphids/testplanner/repository/TestcaseRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestcaseRepository extends JpaRepository<Testcase, Long> {
+
+    public Testcase findByName(String name);
 }
